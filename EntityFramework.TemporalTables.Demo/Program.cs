3a712c7663b5e8091f2c8443601e06b52d6f1264
Mark Raming <Mark.Raming@eraze.com>
2017-04-27 18:18:23 +0200

    Added test code to verify that changing an entity is trival

--- EntityFramework.TemporalTables.Demo/Program.cs
+++ EntityFramework.TemporalTables.Demo/Program.cs
@@ -12,18 +12,31 @@ using EntityFramework.TemporalTable.Demo.DomainEntities;
 namespace EntityFramework.TemporalTable.Demo {
     class Program {
         static void Main(string[] args) {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<TemporalTableDbContext, Configuration>());
+
+            var newId = Guid.NewGuid();
             using (var ctx = new TemporalTableDbContext()) {
+                ctx.Products.Add(new Product { Id = newId, Name = "TestProduct" });
+                ctx.SaveChanges();
+            }
 
-                Database.SetInitializer(new MigrateDatabaseToLatestVersion<TemporalTableDbContext, Configuration>());
-
-                //ctx.Products.Add(new Product { Id = Guid.NewGuid(), Name = "TestProduct" });
-
-                //ctx.SaveChanges();
-
+            using (var ctx = new TemporalTableDbContext()) {
                 var id = new Guid("038fe225-964b-48da-8467-7cce810eea5b");
                 var query = ctx.Products.AsOf(new DateTime(2017, 04, 04, 15, 38, 21)).Where(p => p.Id == id);
 
                 foreach (var p in query) Console.WriteLine($"Name = {p.Name}, EanCode = {p.EanCode}");
+
+                var productToUpdate = ctx.Products.Find(newId);
+                productToUpdate.EanCode = "123456";
+                productToUpdate.Name = "UpdatedTestProductName";
+
+                ctx.SaveChanges();
+            }
+
+            using (var ctx = new TemporalTableDbContext()) {
+                var productToDisplay = ctx.Products.Find(newId);
+                Console.WriteLine(productToDisplay.Name);
+                Console.WriteLine(productToDisplay.EanCode);
             }
         }
     }
